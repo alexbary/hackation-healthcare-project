59c52f5c9a6d1f3d30561dc8003377701fafbdbb
marks6 <mark610@iastate.edu>
2017-10-22 00:48:23 -0500

    Add update index file

--- node-backend/index.js
+++ node-backend/index.js
@@ -1,21 +1,86 @@
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb://nodeRW:ratingDoctors@ds227565.mlab.com:27565/idwapefaprd', function (err, db) {
-  if (err) throw err
 
-  db.collection('doctors').find().toArray(function (err, result) {
+var express = require('express');
+var bodyParser = require('body-parser');
+var app     = express();
+var connectString = 'mongodb://nodeRW:ratingDoctors@ds227565.mlab.com:27565/idwapefaprd';
+const MongoClient = require('mongodb').MongoClient;
+
+//Note that in version 4 of express, express.bodyParser() was
+//deprecated in favor of a separate 'body-parser' module.
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//app.use(express.bodyParser());
+
+
+var db
+MongoClient.connect(connectString, (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(8080, () => {
+    console.log('listening on 8080');
+  })
+});
+
+// app.post('/quotes', (req, res) => {
+//   db.collection('quotes').save(req.body, (err, result) => {
+//     if (err) return console.log(err)
+//     console.log('saved to database')
+//     res.redirect('/')
+//   })
+// })
+
+
+app.get('/finddoctor/:firstname/:lastname', function(req, res) 
+{
+  
+  var doctor = findDoctor();
+
+
+  res.send('hello');
+});
+
+// MongoClient.connect('mongodb://nodeRW:ratingDoctors@ds227565.mlab.com:27565/idwapefaprd', function (err, db) {
+//   if (err) throw err
+
+//   db.collection('doctors').find().toArray(function (err, result) {
+//     if (err) throw err
+
+
+//     console.log(result);
+// 	console.log(result[0].firstName + ' ' + result[0].firstName);
+// 	console.log(result[0].reviews);
+
+// 	var review0 = createReview("cures cancer on sight",0.56);
+// 	var review1 = createReview("fake ass medicine man",-0.44);
+// 	var mark = createDoctor("Mark", "Schwartz", [review0, review1]);
+// 	//db.collection('doctors').insert(mark);
+//   })
+	
+// })
+
+function findDoctor()
+{
+	var returnObject;
+	console.log("1. "+typeof(db));
+	var doctor = db.collection("doctors").find().toArray(function (err, result) {
     if (err) throw err
 
-    console.log(result);
-	console.log(result[0].firstName + ' ' + result[0].firstName);
-	console.log(result[0].reviews);
+    returnObject = result;
 
-	var review0 = createReview("cures cancer on sight",0.56);
-	var review1 = createReview("fake ass medicine man",-0.44);
-	var mark = createDoctor("Mark", "Schwartz", [review0, review1]);
+    //console.log(result);
+	console.log(result[0].firstName + ' ' + result[0].lastName);
+	//console.log(result[0].reviews);
+	return result;
+
+	// var review0 = createReview("cures cancer on sight",0.56);
+	// var review1 = createReview("fake ass medicine man",-0.44);
+	// var mark = createDoctor("Mark", "Schwartz", [review0, review1]);
 	//db.collection('doctors').insert(mark);
-  })
-	
-})
+  });
+	console.log(doctor[0].firstName);
+}
+
+
 
 function createDoctor(fname,lname,reviews){
 
@@ -36,22 +101,3 @@ function createReview(text,sentimentRaw){
 
 	return rating;
 }
-/*
-var express = require('express');
-var bodyParser = require('body-parser');
-var app     = express();
-
-//Note that in version 4 of express, express.bodyParser() was
-//deprecated in favor of a separate 'body-parser' module.
-app.use(bodyParser.urlencoded({ extended: true })); 
-
-//app.use(express.bodyParser());
-
-app.post('/myaction', function(req, res) {
-  res.send('You sent the name "' + req.body.name + '".');
-  console.log(req.body.name);
-});
-
-app.listen(8080, function() {
-  console.log('Server running at http://127.0.0.1:8080/');
-});*/
