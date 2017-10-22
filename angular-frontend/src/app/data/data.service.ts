2e8df804e10db4c6ab20becc379a052b87455fd6
alexbary <absoftwaredev@gmail.com>
2017-10-21 20:18:55 -0500

    Additional improvements to data service

--- angular-frontend/src/app/data/data.service.ts
+++ angular-frontend/src/app/data/data.service.ts
@@ -8,19 +8,25 @@ export class DataService {
 
   	constructor(private http: Http) { }
 
-  	getDoctorsNearby(userLocation: string, radius: string) {
-  		let url = "https://api.betterdoctor.com/2016-03-01/doctors?location="+ userLocation + "%2C" + radius +"&user_location=" + userLocation +"&sort=full-name-asc&skip=0&limit=10&user_key=7c8a24473192660f9cd0fe43b6791bed";
+  	getDoctorsNearby(lat: string, long: string) {
+  		let radius = 50;
+  		let url = "https://api.betterdoctor.com/2016-03-01/doctors?location=" + lat + "%2C" + long + "%2C" + radius + "&user_location=" + lat + "%2C" + long + "&sort=full-name-asc&skip=0&limit=10&user_key=7c8a24473192660f9cd0fe43b6791bed";
     	return this.http.get(url).map((response:Response) => response.json());
   	}
 
   	getSearchResultDoctors(query: string, lat: string, long: string) {
+  		let limit = 99;
   		let radius = 50;
-  		let url = "https://api.betterdoctor.com/2016-03-01/doctors?name=" + query +"&location=" + lat + "%2C" + long + "%2C" + radius + "&user_key=054951f7e3debf472f150c5c85a2d379";
+  		// let url = "https://api.betterdoctor.com/2016-03-01/doctors?name=" + query +"&location=" + lat + "%2C" + long + "%2C" + radius + "&user_key=054951f7e3debf472f150c5c85a2d379";
+    	let url = "https://api.betterdoctor.com/2016-03-01/doctors?name=" + query +"&location=" + lat + "%2C" + long + "%2C" + radius + "&user_location=" + lat + "%2C" + long + "&limit=" + limit + "&user_key=054951f7e3debf472f150c5c85a2d379";
     	return this.http.get(url).map((response:Response) => response.json());
   	}
 
-  	getSearchResultPractices() {
-
+  	getSearchResultPractices(query: string, lat: string, long: string) {
+  		let limit = 99;
+  		let radius = 50;
+  		let url = "https://api.betterdoctor.com/2016-03-01/practices?name=" + query +"&location=" + lat + "%2C" + long + "%2C" + radius + "&user_location=" + lat + "%2C" + long + "&limit=" + limit + "&user_key=054951f7e3debf472f150c5c85a2d379";
+  		return this.http.get(url).map((response:Response) => response.json());
   	}
 
   	getExample() {
