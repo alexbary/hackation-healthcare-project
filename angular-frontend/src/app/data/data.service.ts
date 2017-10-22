6a4d3f8b9a1d80efa003a534faf0275cb51e7172
toepfer <ryantpfr@gmail.com>
2017-10-22 06:21:53 -0500

    css

--- angular-frontend/src/app/data/data.service.ts
+++ angular-frontend/src/app/data/data.service.ts
@@ -13,14 +13,14 @@ export class DataService {
 	});
 
   	getDoctorsNearby(lat: string, long: string) {
-  		let radius = 50;
+  		let radius = 25;
   		let url = "https://api.betterdoctor.com/2016-03-01/doctors?location=" + lat + "%2C" + long + "%2C" + radius + "&user_location=" + lat + "%2C" + long + "&sort=full-name-asc&skip=0&limit=10&user_key=7c8a24473192660f9cd0fe43b6791bed";
     	return this.http.get(url).map((response:Response) => response.json());
   	}
 
   	getSearchResultDoctors(query: string, lat: string, long: string) {
   		let limit = 99;
-  		let radius = 50;
+  		let radius = 25;
   		// let url = "https://api.betterdoctor.com/2016-03-01/doctors?name=" + query +"&location=" + lat + "%2C" + long + "%2C" + radius + "&user_key=054951f7e3debf472f150c5c85a2d379";
     	let url = "https://api.betterdoctor.com/2016-03-01/doctors?name=" + query +"&location=" + lat + "%2C" + long + "%2C" + radius + "&user_location=" + lat + "%2C" + long + "&limit=" + limit + "&user_key=054951f7e3debf472f150c5c85a2d379";
     	return this.http.get(url).map((response:Response) => response.json());
@@ -28,7 +28,7 @@ export class DataService {
 
   	getSearchResultPractices(query: string, lat: string, long: string) {
   		let limit = 99;
-  		let radius = 50;
+  		let radius = 25;
   		let url = "https://api.betterdoctor.com/2016-03-01/practices?name=" + query +"&location=" + lat + "%2C" + long + "%2C" + radius + "&user_location=" + lat + "%2C" + long + "&limit=" + limit + "&user_key=054951f7e3debf472f150c5c85a2d379";
   		return this.http.get(url).map((response:Response) => response.json());
   	}
